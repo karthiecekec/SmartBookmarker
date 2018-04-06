025891b6f4b2be85948b446a17b32c43f1c24f02
Karthikeyan Ramasamy <rkarthik@adobe.com>
2018-04-06 19:38:44 +0530

    second draft

--- background.js
+++ background.js
@@ -4,8 +4,52 @@
 
 'use strict';
 
+/*
 chrome.runtime.onInstalled.addListener( function() {
 	chrome.storage.sync.set({color: '#3aa757'}, function() {
  		console.log("The color is green.");
 	});
+});
+*/
+
+/*
+chrome.browserAction.onClicked.addListener( function(tab) {
+	chrome.tabs.executeScript( tab.id, {
+		code: 'window.getSelection().toString();'
+	}, function(selection) {
+		console.log("Selected Text : " + selection[0]);
+		 $("#selected_text").text = selection[0];
+	});
+});
+*/
+
+chrome.browserAction.onClicked.addListener( function(tab) {
+	chrome.tabs.executeScript(tab.id, {file: 'ui.js'}, function(){}); 
+
+	/*
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		// execute script on current tab only
+
+		tabs.forEach(function(tab) {
+			console.log("tab id: " + tab.id)
+			chrome.tabs.executeScript(tab.id, {file: 'ui.js'}, function(){}); 
+		});
+	});
+	*/
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender) {
+    if( message && message.action == 'get-links') {
+        console.log(message.links);
+    }
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender) {
+    if( message && message.action == 'text-selection') {
+        console.log(message.text);
+         $("#selected_text").text = message.text;
+    }
+    chrome.browserAction.setPopup ({
+ 	popup: "index.html"
+    });
 });
--- ui.js
+++ ui.js
@@ -2,18 +2,40 @@ function receiveText (resultsArray) {
 	 console.log("received the array..");
 }
 
+/*
 chrome.browserAction.onClicked.addListener( function(tab) {
-	console.log('Injecting content script(s)');
-
 	chrome.tabs.executeScript( tab.id, {
 		code: 'window.getSelection().toString();'
 	}, function(selection) {
-		console.log("received the array..");
+		console.log("Selected Text : " + selection[0]);
+		 $("#selected_text").text = selection[0];
 	});
 });
+*/
 
-	chrome.browserAction.setPopup({
-            	tabId: tab.id,
-            	popup: 'index.html'
-        	});
+/*
+chrome.browserAction.onClicked.addListener( function(tab) {
 
+	chrome.browserAction.setPopup ({
+ 		popup: "popup.html"
+ 	});
+
+	var selected_text = window.getSelection().toString();
+	chrome.runtime.sendMessage({action: "text-selection", text: selected_text})
+});
+*/
+
+
+//selected text
+var selected_text = window.getSelection().toString();
+chrome.runtime.sendMessage({action: "text-selection", text: selected_text})
+
+//2 llinks
+var links = [];
+var a = document.querySelectorAll('a');
+for(var i=0; i<2; i++) {
+	links.push(a[i].getAttribute('href'));
+}
+chrome.runtime.sendMessage({action: 'get-links', links: links});
+
+//$("#selected_text").text = "I am here !!";
